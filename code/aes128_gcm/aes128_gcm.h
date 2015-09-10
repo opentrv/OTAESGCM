81d73bac848675b64de9fbc9ddc1f6742acf684b
Denzo77 <Denzo77@users.noreply.github.com>
2015-09-10 12:24:20 +0100

    Update aes128_gcm.h

--- code/aes128_gcm/aes128_gcm.h
+++ code/aes128_gcm/aes128_gcm.h
@@ -1,9 +1,18 @@
+
 /*
- * aes128_gcm.h
- *
- *  Created on: 1 Sep 2015
- *      Author: denzo
- */
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+Author(s) / Copyright (s): Deniz Erbilgin 2015
+*/
 
 /*******************************************************
  * @todo	How To Use This Library
