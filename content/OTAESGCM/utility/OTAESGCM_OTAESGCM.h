f2852e102a78bfeac4d6ee4cb6096d31a16c8fb6
DamonHD <d@hd.org>
2015-09-18 16:30:14 +0100

    Moved comments up.

--- content/OTAESGCM/utility/OTAESGCM_OTAESGCM.h
+++ content/OTAESGCM/utility/OTAESGCM_OTAESGCM.h
@@ -35,11 +35,41 @@ static const uint8_t GCM_BLOCK_SIZE = 16; // GCM block size in bytes. This must 
 static const uint8_t GCM_IV_SIZE    = 12; // GCM initialisation size in bytes.
 static const uint8_t GCM_TAG_SIZE   = 16; // GCM authentication tag size in bytes.
 
+/**
+ * @note
+ * @brief    performs aesgcm encryption
+ * @todo     should this be a void?
+ * @param    key                pointer to 16 byte (128 bit) key
+ * @param    IV                pointer to IV
+ * @param    PDATA            pointer to plaintext array
+ * @param    PDATA_length    length of plaintext array
+ * @param    ADATA             pointer to additional data array
+ * @param    ADATA_length    length of additional data
+ * @param    CDATA            buffer to output ciphertext to
+ * @param    tag                pointer to 16 byte buffer to output tag to
+ */
 bool aes128_gcm_encrypt(    const uint8_t* key, const uint8_t* IV,
                             const uint8_t* PDATA, uint8_t PDATALength,
                             uint8_t* ADATA, uint8_t ADATALength,
                             uint8_t* CDATA, uint8_t *tag);
 
+/**
+ * @note
+ * @brief   performs aesgcm decryption and authentication
+ * @todo    How to do data hiding on authentication fail?
+ *                 - when put into classes?
+ *                 - wipe array?
+ *                 - make PDATA private and then only pass pointer if true?
+ *             Should this be a bool?
+ * @param    key:            pointer to 16 byte (128 bit) key
+ * @param    IV:                pointer to IV
+ * @param    CDATA:          pointer to ciphertext array
+ * @param    CDATALength:   length of ciphertext array
+ * @param    ADATA:          pointer to additional data array
+ * @param    ADATALength:   length of additional data
+ * @param    PDATA:          buffer to output plaintext to
+ * @retval    returns true if authenticated, else returns false
+ */
 uint8_t aes128_gcm_decrypt( const uint8_t* key, const uint8_t* IV,
                             const uint8_t* CDATA, uint8_t CDATALength,
                             const uint8_t* ADATA, uint8_t ADATALength,
