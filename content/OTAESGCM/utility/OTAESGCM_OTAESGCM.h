8963dc51a27e2a2b82e2093c3d55791ff4857c11
DamonHD <d@hd.org>
2017-07-07 13:33:34 +0100

    Adding 'padded' version of function.

--- content/OTAESGCM/utility/OTAESGCM_OTAESGCM.h
+++ content/OTAESGCM/utility/OTAESGCM_OTAESGCM.h
@@ -52,17 +52,30 @@ static constexpr uint8_t AES128GCM_TAG_SIZE   = 16; // GCM authentication tag si
             /**
              * @brief   performs AES-GCM encryption.
              * 			If ADATA unused, set ADATA to NULL and ADATALength to 0.
-             * 			If PDATA unused (This is GMAC), set PDATA and CDATA to NULL and PDATALength to 0.
+             * 			If PDATA unused (this is GMAC),
+             * 			the set PDATA and CDATA to NULL and PDATALength to 0.
              * @todo	Make GMAC helper function.
              * @param   key		pointer to 16 byte (128 bit) key; never NULL
-             * @param   IV             	pointer to 12 byte (96 bit) IV; never NULL
-             * @param   PDATA          	pointer to plaintext array, this is internally padded up to a multiple of the blocksize; NULL if length 0.
-             * @param   PDATALength	length of plaintext array in bytes, can be zero
-             * @param   ADATA           pointer to additional data array; NULL if length 0.
-             * @param   ADATALength    	length of additional data in bytes, can be zero
-             * @param   CDATA           buffer to output ciphertext to, size MUST BE PADDED/EXPANDED TO FULL BLOCKSIZE MULTIPLE at/above PDATAlength;
-             *                          (nominally set to NULL if PDATA is NULL but seems to cause a crash)
-             * @param   tag             pointer to 16 byte buffer to output tag to; never NULL
+             * @param   IV             	pointer to 12 byte (96 bit) IV;
+             *                          never NULL
+             * @param   PDATA          	pointer to plaintext input array,
+             *                          this is internally padded up to
+             *                          a multiple of the blocksize;
+             *                          NULL if length 0.
+             * @param   PDATALength	length of plaintext array in bytes,
+             *                          can be zero,
+             *                          need not be blocksize multiple.
+             * @param   ADATA           pointer to additional input data array;
+             *                          NULL if length 0.
+             * @param   ADATALength    	length of additional data in bytes,
+             *                          can be zero
+             * @param   CDATA           buffer to output ciphertext to,
+             *                          size MUST BE PADDED/EXPANDED TO FULL
+             *                          BLOCKSIZE MULTIPLE at/above PDATAlength;
+             *                          (nominally set to NULL if PDATA is NUL
+             *                          but seems to cause a crash)
+             * @param   tag             pointer to 16 byte tag output buffer;
+             *                          never NULL
              * @retval	true if encryption is successful, else false
              */
             virtual bool gcmEncrypt(
@@ -70,6 +83,45 @@ static constexpr uint8_t AES128GCM_TAG_SIZE   = 16; // GCM authentication tag si
                 const uint8_t* PDATA, uint8_t PDATALength,
                 const uint8_t* ADATA, uint8_t ADATALength,
                 uint8_t* CDATA, uint8_t *tag) const = 0;
+
+            /**
+             * @brief   performs AES-GCM encryption on padded data.
+             *          If ADATA unused, set ADATA to NULL and ADATALength to 0.
+             *          If PDATA unused (this is GMAC),
+             *          then set PDATA and CDATA to NULL and PDATALength to 0.
+             * @todo    Make GMAC helper function.
+             * @param   key     pointer to 16 byte (128 bit) key; never NULL
+             * @param   IV              pointer to 12 byte (96 bit) IV;
+             *                          never NULL
+             * @param   PDATA           pointer to plaintext input array,
+             *                          MUST BT a multiple of the blocksize;
+             *                          NULL if length 0.
+             * @param   PDATALength     length of plaintext array in bytes,
+             *                          can be zero,
+             *                          MUST BE blocksize multiple.
+             * @param   ADATA           pointer to additional input data array;
+             *                          NULL if length 0.
+             * @param   ADATALength     length of additional data in bytes,
+             *                          can be zero
+             * @param   CDATA           buffer to output ciphertext to,
+             *                          size MUST BE PADDED/EXPANDED TO FULL
+             *                          BLOCKSIZE MULTIPLE at/above PDATAlength;
+             *                          (nominally set to NULL if PDATA is NULL
+             *                          but seems to cause a crash)
+             * @param   tag             pointer to 16 byte tag output buffer;
+             *                          never NULL
+             * @retval  true if encryption is successful, else false
+             *
+             * Plain-text must be an exact multiple of block length, eg padded.
+             * This version may be smaller and faster and need less stack
+             * if separately implemented, else default to generic gcmEncrypt().
+             */
+            virtual bool gcmEncryptPadded(
+                const uint8_t* key, const uint8_t* IV,
+                const uint8_t* PDATAPadded, uint8_t PDATALength,
+                const uint8_t* ADATA, uint8_t ADATALength,
+                uint8_t* CDATA, uint8_t *tag) const
+                { return(gcmEncrypt(key, IV, PDATAPadded, PDATALength, ADATA, ADATALength, CDATA, tag)); }
 
             /**
              * @brief   performs AES-GCM decryption and authentication
@@ -113,12 +165,24 @@ static constexpr uint8_t AES128GCM_TAG_SIZE   = 16; // GCM authentication tag si
             // but may hold temporary workspace or non-key/data-dependent state.
             constexpr OTAES128GCMGenericBase(OTAES128E *aptr) : ap(aptr) { }
             // Encrypt; true iff successful.
+            // Plain text need not be padded to a block-size multiple.
             virtual bool gcmEncrypt(
                 const uint8_t* key, const uint8_t* IV,
                 const uint8_t* PDATA, uint8_t PDATALength,
                 const uint8_t* ADATA, uint8_t ADATALength,
                 uint8_t* CDATA, uint8_t *tag) const override;
+            // Encrypt; true iff successful.
+            // Plain-text must be an exact multiple of block length, eg padded.
+            // This version may be smaller and faster and need less stack
+            // if separately implemented,
+            // else default to
+            virtual bool gcmEncryptPadded(
+                const uint8_t* key, const uint8_t* IV,
+                const uint8_t* PDATAPadded, uint8_t PDATALength,
+                const uint8_t* ADATA, uint8_t ADATALength,
+                uint8_t* CDATA, uint8_t *tag) const override;
             // Decrypt; true iff successful.
+            // Crypto text must always be a multiple of block length.
             virtual bool gcmDecrypt(
                  const uint8_t* key, const uint8_t* IV,
                  const uint8_t* CDATA, uint8_t CDATALength,
