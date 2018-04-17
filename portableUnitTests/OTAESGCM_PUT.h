6ee139826bb7006a66ab6e42e4568384c2d94896
DamonHD <d@hd.org>
2018-04-17 15:34:12 +0100

    inter-lib PUT support hooks...

---
+++ portableUnitTests/OTAESGCM_PUT.h
@@ -0,0 +1,24 @@
+/*
+The OpenTRV project licenses this file to you
+under the Apache Licence, Version 2.0 (the "Licence");
+you may not use this file except in compliance
+with the Licence. You may obtain a copy of the Licence at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the Licence is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied. See the Licence for the
+specific language governing permissions and limitations
+under the Licence.
+
+Author(s) / Copyright (s): Damon Hart-Davis 2018
+*/
+
+/*
+ * OTAESGCM available external routines for Portable Unit Tests.
+ */
+
+
+/* ... crickets ... */
